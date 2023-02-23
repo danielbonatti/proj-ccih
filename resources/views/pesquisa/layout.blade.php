2ab9687f875a926486af8967a0735b303d1f18d8
danielbonatti <bonattidaniel@hotmail.com>
2023-02-23 17:11:01 -0300

    ordenacao okay 2

--- resources/views/pesquisa/layout.blade.php
+++ resources/views/pesquisa/layout.blade.php
@@ -39,7 +39,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         
-        <script src="{{ asset('js/tablesort.min.js') }}"></script>
+        <script src="{{ asset('public/js/tablesort.min.js') }}"></script>
 
         <script>
             $(document).ready(function(){               
